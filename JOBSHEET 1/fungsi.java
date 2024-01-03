b525aef71d992756bc28420a0365726a923b660b
Kaylars <satitiputri.kayla@gmail.com>
2024-01-03 22:18:56 +0700

    add 2 fungsi

--- JOBSHEET 1/fungsi.java
+++ JOBSHEET 1/fungsi.java
@@ -3,23 +3,46 @@ public class fungsi {
     //  fungsi untuk menampilkan array stock bunga
     static void stokBunga() {
         String[][] flowerStocks = {
-            {"            ", "Aglonema", "Keladi  ", "Alocasia", "Mawar"},
-            {"RoyalGarden1", "   10   ", "   5    ", "   15   ", "  7 "},
-            {"RoyalGarden2", "    6   ", "  11    ", "    9   ", " 12 "},
-            {"RoyalGarden3", "    2   ", "  10    ", "   10   ", "  5 "},
-            {"RoyalGarden4", "    5   ", "   7    ", "   12   ", "  9 "}
+            {"-------------------------------------------------------"},
+            {"             |", " Aglonema  |", "Keladi |", "Alocasia |", "Mawar |"},
+            {"-------------|------------|--------|----------|-------|"},
+            {"RoyalGarden1 |", "    10     |", "   5   |", "   15    |", "  7   |"},
+            {"-------------|------------|--------|----------|-------|"},
+            {"RoyalGarden2 |", "     6     |", "  11   |", "    9    |", " 12   |"},
+            {"-------------|------------|--------|----------|-------|"},
+            {"RoyalGarden3 |", "     2     |", "  10   |", "   10    |", "  5   |"},
+            {"-------------|------------|--------|----------|-------|"},
+            {"RoyalGarden4 |", "     5     |", "   7   |", "   12    |", "  9   |"},
+            {"-------------|------------|--------|----------|-------|"},
         };
 
         System.out.println();
 
         for (String[] flowerStock : flowerStocks) {
             for (String stock : flowerStock) {
-                System.out.print(stock + "\t");
+                System.out.print(stock + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // fungsi untuk mengetahui jumlah stok tiap bunga di seluruh cabang
+    static void jmlStok() {
+        String[][] jumlahStok = {
+            {"   TOTAL     |", "    23     |", "  33   |", "   46    |", " 33   |"},
+            {"-------------------------------------------------------"}
+        };
+
+        for (String[] jmlStok : jumlahStok) {
+            for (String stok : jmlStok) {
+                System.out.print(stok + " ");
             }
             System.out.println();
         }
     }
     public static void main(String[] args) {
         stokBunga();
+        jmlStok();
+        System.out.println();
     }
 }
