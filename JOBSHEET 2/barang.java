a544455a87d2a11ce62b99e082a15657c533607b
Kaylars <satitiputri.kayla@gmail.com>
2024-01-06 18:51:05 +0700

    add

---
+++ JOBSHEET 2/barang.java
@@ -0,0 +1,25 @@
+public class barang {
+    String namaBarang, jenisBarang;
+    int stok, hargaSatuan;
+
+    void tampilBarang() {
+        System.out.println("Nama barang  : " + namaBarang);
+        System.out.println("Jenis barang : " + jenisBarang);
+        System.out.println("Stok barang  : " + stok);
+        System.out.println("Harga satuan : " + hargaSatuan);
+    }
+
+    void tambahStok(int n ) {
+        stok = stok + n;
+    }
+
+    void kurangiStok(int n) {
+        if (stok > 0) {
+            stok = stok - n;
+        }
+    }
+
+    int hitungHargaTotal(int jumlah) {
+        return jumlah * hargaSatuan;
+    }
+}
