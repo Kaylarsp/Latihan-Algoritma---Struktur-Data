d10e1c7c4bb22ecf2d056ac9ff26cb31557f492c
Kaylars <satitiputri.kayla@gmail.com>
2024-01-07 22:18:11 +0700

    menerima input menggunakan looping

--- JOBSHEET 3/PersegiPanjang.java
+++ JOBSHEET 3/PersegiPanjang.java
@@ -1,23 +1,42 @@
+import java.util.Scanner;
+
 class ArrayObject {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         PersegiPanjang[] ppArray = new PersegiPanjang[3];
+
+        // menerima input
+        for (int i = 0; i < 3; i++) {
+            ppArray[i] = new PersegiPanjang();
+            System.out.println("\nPersegi panjang ke-" + i);
+            System.out.print("Masukkan panjang : ");
+            ppArray[i].panjang = sc.nextInt();
+            System.out.print("Masukkan lebar   : ");
+            ppArray[i].lebar = sc.nextInt();
+        }
+
+        // mengakses isi array ppArray dan menampilkannya
+        for (int i = 0; i < 3; i++) {
+            System.out.println("\nPersegi panjang ke-" + i);
+            System.out.println("Panjang : " + ppArray[i].panjang + "\nLebar   : " + ppArray[i].lebar);
+        }
     
-        ppArray[0] = new PersegiPanjang();
-        ppArray[0].panjang = 110;
-        ppArray[0].lebar = 30;
+        // ppArray[0] = new PersegiPanjang();
+        // ppArray[0].panjang = 110;
+        // ppArray[0].lebar = 30;
     
-        ppArray[1] = new PersegiPanjang();
-        ppArray[1].panjang = 80;
-        ppArray[1].lebar = 40;
+        // ppArray[1] = new PersegiPanjang();
+        // ppArray[1].panjang = 80;
+        // ppArray[1].lebar = 40;
     
-        ppArray[2] = new PersegiPanjang();
-        ppArray[2].panjang = 100;
-        ppArray[2].lebar = 20;
+        // ppArray[2] = new PersegiPanjang();
+        // ppArray[2].panjang = 100;
+        // ppArray[2].lebar = 20;
     
-        System.out.println();
-        System.out.println("Persegi panjang ke-0, panjang : " + ppArray[0].panjang + ", lebar : " + ppArray[0].lebar);
-        System.out.println("Persegi panjang ke-1, panjang :  " + ppArray[1].panjang + ", lebar : " + ppArray[1].lebar);
-        System.out.println("Persegi panjang ke-2, panjang : " + ppArray[2].panjang + ", lebar : " + ppArray[2].lebar);
+        // System.out.println();
+        // System.out.println("Persegi panjang ke-0, panjang : " + ppArray[0].panjang + ", lebar : " + ppArray[0].lebar);
+        // System.out.println("Persegi panjang ke-1, panjang :  " + ppArray[1].panjang + ", lebar : " + ppArray[1].lebar);
+        // System.out.println("Persegi panjang ke-2, panjang : " + ppArray[2].panjang + ", lebar : " + ppArray[2].lebar);
     }
 }
 
