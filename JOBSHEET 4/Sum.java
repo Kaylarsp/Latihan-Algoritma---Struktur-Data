ac768c1f15e214f58ed37b8d5fccb056285893c2
Kaylars <satitiputri.kayla@gmail.com>
2024-01-15 23:27:40 +0700

    update w/ formatting

--- JOBSHEET 4/Sum.java
+++ JOBSHEET 4/Sum.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Sum {
     
@@ -31,6 +32,11 @@ public class Sum {
         }
         return 0;
     }
+
+    public String formatDouble(double number) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        return df.format(number);
+    }
 }
 
 class MainSum {
@@ -50,9 +56,11 @@ class MainSum {
 
         System.out.println("===============================================================");
         System.out.println("Algoritma Brute Force");
-        System.out.println("Total keuntungan perusahaan selama " + sm.elemen + " bulan adalah : " + sm.totalBF(sm.keuntungan));
+        double formattedBF = Double.parseDouble(sm.formatDouble(sm.totalBF(sm.keuntungan)));
+        System.out.println("Total keuntungan perusahaan selama " + sm.elemen + " bulan adalah : " + formattedBF);
         System.out.println("===============================================================");
         System.out.println("Algoritma Divide and Conquer");
-        System.out.println("Total keuntungan perusahaan selama " + sm.elemen + " bulan adalah : " + sm.totalDC(sm.keuntungan, 0, sm.elemen-1));
+        double formattedDC = Double.parseDouble(sm.formatDouble(sm.totalDC(sm.keuntungan, 0, sm.elemen-1)));
+        System.out.println("Total keuntungan perusahaan selama " + sm.elemen + " bulan adalah : " + formattedDC);
     }
 }
