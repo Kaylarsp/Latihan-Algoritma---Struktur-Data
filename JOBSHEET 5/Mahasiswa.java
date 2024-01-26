5039107f1677f09e4a6b67d17540ad2eefadf544
Kaylars <satitiputri.kayla@gmail.com>
2024-01-26 20:58:00 +0700

    percobaan 1 done

--- JOBSHEET 5/Mahasiswa.java
+++ JOBSHEET 5/Mahasiswa.java
@@ -16,4 +16,60 @@ public class Mahasiswa {
         System.out.println("Umur        : " + umur);
         System.out.println("IPK         : " + ipk);
     }
+
+    public static void main(String[] args) {
+        DaftarMahasiswaBerprestasi list = new DaftarMahasiswaBerprestasi();
+        Mahasiswa m1 = new Mahasiswa("Nusa", 2017, 25, 3);
+        Mahasiswa m2 = new Mahasiswa("Rara", 2012, 19, 4);
+        Mahasiswa m3 = new Mahasiswa("Dompu", 2018, 19, 3.5);
+        Mahasiswa m4 = new Mahasiswa("Abdul", 2017, 23, 2);
+        Mahasiswa m5 = new Mahasiswa("Ummi", 2019, 21, 3.75);
+
+        list.tambah(m1);
+        list.tambah(m2);
+        list.tambah(m3);
+        list.tambah(m4);
+        list.tambah(m5);
+
+        System.out.println("\nData mahasiswa sebelum sorting : \n");
+        list.tampil();
+
+        System.out.println("\nData mahasiswa setelah sorting desc berdasarkan ipk : \n");
+        list.bubbleSort();
+        list.tampil();
+    }
+}
+
+class DaftarMahasiswaBerprestasi {
+    Mahasiswa listMhs[] = new Mahasiswa[5];
+    int idx;
+
+    void tambah(Mahasiswa m){
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!!");
+        }
+    }
+
+    void tampil(){
+        for (Mahasiswa m : listMhs) {
+            m.tampil();
+            System.out.println("-----------------------");
+        }
+    }
+
+    void bubbleSort(){
+        for (int i = 0; i < listMhs.length - 1; i++) {
+            for (int j = 1; j < listMhs.length - i; j++) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                    // proses swap atau penukaran
+                    Mahasiswa tmp = listMhs[j];
+                    listMhs[j] = listMhs[j - 1];
+                    listMhs[j - 1] = tmp;
+                }
+            }
+        }
+    }
 }
