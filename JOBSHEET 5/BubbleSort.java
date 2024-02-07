fdeb71c568e3257a7a934d7d51e6e952b67d999f
Kaylars <satitiputri.kayla@gmail.com>
2024-02-07 16:30:00 +0700

    + explanation

--- JOBSHEET 5/BubbleSort.java
+++ JOBSHEET 5/BubbleSort.java
@@ -38,3 +38,37 @@ public class BubbleSort {
         }
     }
 }
+
+// misal e var numbers iku [5,4,3,2,1] brarti length e kan 5
+
+// nde awal definisi var swapped value ne true.
+// terus njalanno while ketika swapped bernilai true
+
+// nde njero while, teko awal swapped e di replace dadi false
+
+// terus menjalankan perulangan for(literasi sebanyak 4x (5-1))
+
+
+// #literasi 1
+
+// terus ono kondisi if numbers[i] > numbers[i+1]
+
+// numbers[i] > numbers[i+1]
+//    5	   >      4
+
+// jika true maka swapped sing bar di replace false, saiki direplace dadi true maneh.
+// terus ono inisialisasi var temp = numbers[i];
+
+// //fungsi var temp iki gawe menyimpan sementara
+// var temp = numbers[i];
+// var temp = numbers[0] => 5
+
+// // iki operasi sorting
+// numbers[i] = numbers[i+1];
+// numbers[0] =  numbers[0+1] => numbers[1] => 4
+
+// // iki finalisasi sorting
+// numbers[i+1] = temp;
+// numbers[0+1] => numbers[1] = 5
+
+// nah iki kabeh engko di ulang sampe swapped bernilai false dan terakhir pasti hasil e var number dadi [1,2,3,4,5]
