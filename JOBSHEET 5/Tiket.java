9da7036d79f2269d54c7e23e60eee9194e458b77
Kaylars <satitiputri.kayla@gmail.com>
2024-02-09 14:43:32 +0700

    implementasi sesuai diagram

---
+++ JOBSHEET 5/Tiket.java
@@ -0,0 +1,37 @@
+public class Tiket {
+    String maskapai, asal, tujuan;
+    int harga;
+
+    void Tiket(String m, String a, String t, int h) {
+        maskapai = m;
+        asal = a;
+        tujuan = t;
+        harga = h;
+    }
+
+    class TiketService {
+        Tiket tikets[] = new Tiket[5];
+
+        void tambah(Tiket t) {
+
+        }
+
+        void tampilAll() {
+
+        }
+
+        void bubbleSort() {
+
+        }
+
+        void selectionSort() {
+
+        }
+    }
+
+    class MainTiket {
+        public static void main(String[] args) {
+
+        }
+    }
+}
